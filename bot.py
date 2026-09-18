4dfec10f756b7d3880f9bbc9787504271adbf8a3
Mahitha <mahitha.chippa05@gmail.com>
2026-09-18 12:50:18 +0530

    checking if updater script works or not

--- bot.py
+++ bot.py
@@ -105,7 +105,7 @@ def date_error():
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     message = (
-        "🤖 Personal Server of Kush Modi\n\n"
+        "🤖 Personal Server\n\n"
 
         "🍽 FOOD\n"
         "/food NAME CALORIES\n"
